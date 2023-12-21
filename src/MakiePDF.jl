caa87a3b11f4a298da926fdcf0eeed4308a891b7
daizutabi <daizutabi@gmail.com>
2023-12-22 07:48:10 +0900

    text/html: unshowable

--- src/MakiePDF.jl
+++ src/MakiePDF.jl
@@ -4,6 +4,7 @@ using IJulia
 using Makie
 
 Base.showable(::MIME"application/pdf", ::Makie.FigureLike) = true
+Base.showable(::MIME"text/html", ::Makie.FigureLike) = false
 
 __init__() = IJulia.register_mime(MIME("application/pdf"))
 
