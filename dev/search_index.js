e90fb387a7ff88856d2f59b5a3fbb4bc1e9a14dd
Documenter.jl <documenter@juliadocs.github.io>
2023-12-22 11:27:52 +0000

    build based on bf96866

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MakiePDF","category":"page"},{"location":"#MakiePDF","page":"Home","title":"MakiePDF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MakiePDF.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MakiePDF]","category":"page"}]
+}
